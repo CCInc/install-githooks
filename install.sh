3e97549f251e6a8cf56471d68234166ceb16603d
Christopher Lee <csl170000@utdallas.edu>
2022-08-16 16:03:39 -0600

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,19 @@ if [ ! -d ".git/hooks" ]; then
     exit 1
 fi
 
-curl https://raw.githubusercontent.com/CCInc/install-githooks/main/hooks/commit-msg --output .git/hooks/commit-msg
-chmod +x .git/hooks/commit-msg
+for HOOK in commit-msg
+do
+    if [ -d ".git/hooks/$HOOK" ]; then
+        read -p "Warning! The hook $HOOK already exists, do you want to override it? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            rm .git/hooks/$HOOK
+        fi
+    fi
+    
+    curl https://raw.githubusercontent.com/CCInc/install-githooks/main/hooks/$HOOK --output .git/hooks/$HOOK
+    chmod +x .git/hooks/$HOOK
+done
 
 echo "Successfully installed hooks"
